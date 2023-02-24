e36b546b095705b4fbfdefcde4e313014d4448c9
Martin Stewart <m.stewart796@gmail.com>
2023-02-24 15:59:04 -0300

    Completed first exercise

---
+++ 1.c
@@ -0,0 +1,16 @@
+// Faça um programa que leia um número inteiro e o imprima.
+
+#include <stdio.h>
+
+int main(){
+
+    int numero;
+    printf("Informe o número: ");
+    scanf("%d", &numero);
+    printf("\nO número foi: %d", numero);  
+
+    return 0; 
+
+}
+
+
