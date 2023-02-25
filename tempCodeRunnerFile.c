e00c5386d798b35e01cf951ddefa21d064531c45
Martin Stewart <m.stewart796@gmail.com>
2023-02-25 13:46:43 -0300

    Completed exercise 7

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,17 @@
+// Leia um valor em real e a cotação do dólar. Em seguida, imprima o valor
+// correspondente em dólares.
 
+#include <stdio.h>
+
+void main(){
+
+    float real, cota, dolar;
+    printf("Informe o valor em real: R$");
+    scanf("%f", &real);
+    printf("Informe a cotação: ");
+    scanf("%f", &cota);
+
+    dolar = real * cota;
+    printf("O valor de R$%.2f hoje é: $%.2f\n", real, dolar);
+    
+}
