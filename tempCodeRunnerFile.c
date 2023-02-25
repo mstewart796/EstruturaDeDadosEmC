8cb9ce46603069c543ade9c76cc45b8ccfa03f97
Martin Stewart <m.stewart796@gmail.com>
2023-02-25 14:13:58 -0300

    Completed exercise 10

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,20 @@
-// A importância de R$ 780.000,00 será dividida entre três ganhadores de
-// um concurso. Sendo que da quantia total:
-// - O primeiro ganhador receberá 46%;
-// - O segundo receberá 32%;
-// - O terceiro receberá o restante;
-// Calcule e imprima a quantia ganha por cada um dos ganhadores.
+// Faça um programa que leia um número e, caso ele seja positivo, calcule e
+// mostre:
+// - O número digitado ao quadrado
+// - A raiz quadrada do número digitado
 
-#include <stdio.h> 
+#include <stdio.h>
+#include <math.h>
 
 void main(){
 
-    float valor = 780000, primeiro = valor * 0.46, segundo = valor * 0.32, terceiro = valor * (1 - (0.46 + 0.32));
-    printf("O primeiro receberá: R$%.2f\n\
-    O segundo receberá: R$%.2f\n\
-    O terceiro receberá: R$%.2f\n", primeiro, segundo, terceiro);
+    float numero, quadrado, sqRoot;
 
+    printf("Informe o número: ");
+    scanf("%f", &numero);
+
+    quadrado = numero * numero;
+    sqRoot = sqrt(numero);
+
+    printf("%f quadrado é %f e a raiz quadrado dele é %f\n", numero, quadrado, sqRoot);
 }
