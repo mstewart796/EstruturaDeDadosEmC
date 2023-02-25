c61781fdc5f16e0ee47b9a77dc1046ee2786e6ac
Martin Stewart <m.stewart796@gmail.com>
2023-02-25 13:42:10 -0300

    Completed exercise 6

---
+++ 6.c
@@ -0,0 +1,24 @@
+// Leia quatro notas, calcule a média aritmética e imprima o resultado.
+
+#include <stdio.h>
+
+void main(){
+
+    float N1, N2, N3, N4, media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &N1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &N2);
+
+    printf("Digite a terceira nota: ");
+    scanf("%f", &N3);
+
+    printf("Digite a quarta nota: ");
+    scanf("%f", &N4);
+
+    media = (N1 + N2 + N3 + N4) / 4;
+    printf("A média é: %.1f\n", media);
+
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,17 +1,1 @@
-// Leia uma velocidade em km/h (quilômetros por hora) e apresente-a
-// convertida em m/s (metros por segundo). A fórmula de conversão é: M =
-// K/3.6, sendo K a velocidade em km/h e M em m/s.
 
-#include <stdio.h>
-
-void main(){
-
-    int K;
-
-    printf("Informe a velocidade em km/h: ");
-    scanf("%d", &K);
-
-    float M = K/3.6;
-
-    printf("%ikm/h é igual a %.2fm/s\n", K, M);
-}
