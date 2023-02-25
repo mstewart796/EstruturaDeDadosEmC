8cdf3d7ff307c10512d31af6d95ac84793a5b032
Martin Stewart <m.stewart796@gmail.com>
2023-02-25 13:57:31 -0300

    Completed exercise 9

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,17 @@
-// 8. Leia o valor do raio de um círculo e calcule e imprima a área do círculo
-// correspondente. A área do círculo é π*r² , considere π = 3.141592.
+// A importância de R$ 780.000,00 será dividida entre três ganhadores de
+// um concurso. Sendo que da quantia total:
+// - O primeiro ganhador receberá 46%;
+// - O segundo receberá 32%;
+// - O terceiro receberá o restante;
+// Calcule e imprima a quantia ganha por cada um dos ganhadores.
 
-#include <stdio.h>
+#include <stdio.h> 
 
-void main() {
+void main(){
 
-    float raio, pi = 3.141592, area;
-    printf("Informe o valor do raio: ");
-    scanf("%f", &raio);
-
-    area = (raio * raio) * pi;
-    printf("A area do círculo é: %f\n", area);
+    float valor = 780000, primeiro = valor * 0.46, segundo = valor * 0.32, terceiro = valor * (1 - (0.46 + 0.32));
+    printf("O primeiro receberá: R$%.2f\n\
+    O segundo receberá: R$%.2f\n\
+    O terceiro receberá: R$%.2f\n", primeiro, segundo, terceiro);
 
 }
