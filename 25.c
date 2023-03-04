2059e21d232fad65db65d0724bd6dc8f79e22eb7
mstewart796 <m.stewart796@gmail.com>
2023-03-04 13:44:54 -0300

    Started ex. 25

---
+++ 25.c
@@ -0,0 +1,5 @@
+//  Faça um programa que receba dois números. Calcule e mostre:
+// - a soma dos números pares desse intervalo de números, incluindo os
+// números digitados;
+// - a multiplicação dos números ímpares desse intervalo, incluindo os
+// digitados;
