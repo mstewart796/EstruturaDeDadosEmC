46641158bf67855d32bf7b4a4445ba2aa576bfc0
mstewart796 <m.stewart796@gmail.com>
2023-03-04 13:54:46 -0300

    Completed ex. 25

--- 25.c
+++ 25.c
@@ -15,11 +15,20 @@ void main() {
     printf("Informe numero 2: ");
     scanf("%d", &numero2);
 
-    int soma = 0;
+    // inicialize sum as 0 but multi as 1 otherwise multi will stay as 0
+    int soma = 0, multi = 1;
 
     for (int i = numero1; i <= numero2; i++) {
+        // if to check if even    
         if (i % 2 == 0) {
+            // if so, add to sum
             soma += i;
+        } else {
+            // else multiply the odd numbers
+            multi *= i;
         }
     }
+
+    printf("A soma dos numeros pares (inclusivos) entre %d e %d eh: %d\n", numero1, numero2, soma);
+    printf("A multiplicacao dos numeros impares (inclusivos) entre este mesmo intervalo eh %d", multi);
 }
