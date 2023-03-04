def6f8e91208ccf90ccb2301f50baf5b521e3214
mstewart796 <m.stewart796@gmail.com>
2023-03-03 22:36:05 -0300

    Completed ex. 22

--- 22.c
+++ 22.c
@@ -4,4 +4,15 @@
  #include <stdio.h>
 
  void main() {
+
+    int N;
+
+    printf("Informe o valor de N: ");
+    scanf("%d", &N);
+
+    for (int i = 0; i < N * 2; i++) {
+        if (i % 2 != 0) {
+            printf("%d\n", i);
+        }
+    }
  }
