33cda1983f8e8120e1648ba475aa7dcaddd1b244
mstewart796 <m.stewart796@gmail.com>
2023-03-03 22:40:03 -0300

    Completed ex. 23

--- 23.c
+++ 23.c
@@ -1,3 +1,16 @@
 // Faça um programa que leia um numero inteiro positivo “N” e imprima
 // todos os números naturais de 0 até “N” em ordem crescente.
 
+#include <stdio.h>
+
+void main() {
+
+    int N;
+
+    printf("Informe o valor de N: ");
+    scanf("%d", &N);
+
+    for (int i = 0; i <= N; i++) {
+        printf("%d\n", i);
+    }
+}
