c4a7589202dc7cdcb1566622ebfe7287a8362fa2
mstewart796 <m.stewart796@gmail.com>
2023-03-06 21:27:17 -0300

    Completed ex. 26

--- 26.c
+++ 26.c
@@ -4,6 +4,7 @@
 
 void main() {
 
+    // * 1
     printf("-------------\n");
     printf("1 timestable\n");
     printf("-------------\n");
@@ -11,6 +12,7 @@ void main() {
         printf("1 * %d = %d\n", i, 1 * i);
     }
 
+    // * 2
     printf("-------------\n");
     printf("2 timestable\n");
     printf("-------------\n");
@@ -19,11 +21,64 @@ void main() {
         printf("2 * %d = %d\n", i, 2 * i);
     }
 
+    // * 3
     printf("-------------\n");
     printf("3 timestable\n");
     printf("-------------\n");
     
     for (int i = 1; i <= 10; i++) {
         printf("3 * %d = %d\n", i, 3 * i);
+    }
+    
+    // * 4
+    printf("-------------\n");
+    printf("4 timestable\n");
+    printf("-------------\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("4 * %d = %d\n", i, 4 * i);
+    }
+
+    // * 5
+    printf("-------------\n");
+    printf("5 timestable\n");
+    printf("-------------\n");
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("5 * %d = %d\n", i, 5 * i);
+    }
+
+    // * 6
+    printf("-------------\n");
+    printf("6 timestable\n");
+    printf("-------------\n");
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("6 * %d = %d\n", i, 6 * i);
+    }
+
+    // * 7
+    printf("-------------\n");
+    printf("7 timestable\n");
+    printf("-------------\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("7 * %d = %d\n", i, 7 * i);
+    }
+
+    // * 8
+    printf("-------------\n");
+    printf("8 timestable\n");
+    printf("-------------\n");
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("8 * %d = %d\n", i, 8 * i);
+    }
+
+    // * 9
+    printf("-------------\n");
+    printf("9 timestable\n");
+    printf("-------------\n");
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("9 * %d = %d\n", i, 9 * i);
     }
 }
