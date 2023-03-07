f81792c4dcb841c41b489fb7a0a96c37d067e95e
mstewart796 <m.stewart796@gmail.com>
2023-03-06 21:24:10 -0300

    Completed 3 timestable

--- 26.c
+++ 26.c
@@ -4,8 +4,26 @@
 
 void main() {
 
+    printf("-------------\n");
+    printf("1 timestable\n");
+    printf("-------------\n");
     for (int i = 1; i <= 10; i++) {
         printf("1 * %d = %d\n", i, 1 * i);
     }
+
+    printf("-------------\n");
+    printf("2 timestable\n");
+    printf("-------------\n");
     
+    for (int i = 1; i <= 10; i++) {
+        printf("2 * %d = %d\n", i, 2 * i);
+    }
+
+    printf("-------------\n");
+    printf("3 timestable\n");
+    printf("-------------\n");
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("3 * %d = %d\n", i, 3 * i);
+    }
 }
