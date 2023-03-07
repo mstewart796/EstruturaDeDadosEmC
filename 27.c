05597c04295b0148761884218aa36f6bcb52e25f
mstewart796 <m.stewart796@gmail.com>
2023-03-06 21:38:30 -0300

    Set up ex. 27

---
+++ 27.c
@@ -0,0 +1,9 @@
+//  Escreva um programa que leia um numero inteiro positivo “N” e em
+// seguida imprima “N” linhas do chamado Triângulo de Floyd. Para n = 6,
+// temos:
+
+#include <stdio.h>
+
+void main() {
+
+}
