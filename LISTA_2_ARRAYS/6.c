5d4569ab57ac34bb27c0210aa490207f40011e62
mstewart796 <m.stewart796@gmail.com>
2023-03-11 19:34:42 -0300

    Set up ex. 6

---
+++ LISTA_2_ARRAYS/6.c
@@ -0,0 +1,8 @@
+// Escreva um programa que leia 10 números inteiros e os armazene em um vetor. Imprima 
+// o vetor, o menor elemento e a posição que ele se encontra.
+
+#include <stdio.h>
+
+void main() {
+    
+}
