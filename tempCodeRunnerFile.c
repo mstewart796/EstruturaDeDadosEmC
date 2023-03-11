099de573fba066c45f2413803a20f0e7c1923bf2
mstewart796 <m.stewart796@gmail.com>
2023-03-11 17:59:52 -0300

    Finished first for loop ex.2

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+// Crie um programa que lê 5 valores inteiros e, em seguida, mostre na tela os valores lidos. 
+
+#include <stdio.h>
+
+void main() {
+
+    int array[5];
+    for (int i = 0; i < 5; i++) {
+        printf("Informe o %dº numero: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+}
