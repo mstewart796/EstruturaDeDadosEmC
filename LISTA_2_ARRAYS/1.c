a06556ee819ced83bc03c240045f8f2488198804
mstewart796 <m.stewart796@gmail.com>
2023-03-11 16:24:57 -0300

    Set up ex.1

--- LISTA_2_ARRAYS/1.c
+++ LISTA_2_ARRAYS/1.c
@@ -1,4 +1,8 @@
 // Faça um programa que possua um vetor denominado A que armazene 6 números inteiros. 
 // O programa deve executar os seguintes passos: 
  
- 
+ #include <stdio.h>
+
+ void main() {
+    
+ }
