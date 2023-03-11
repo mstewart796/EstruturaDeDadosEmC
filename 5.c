34d3dc6b9d479d1572c539d102bbac2de8cb3b60
mstewart796 <m.stewart796@gmail.com>
2023-03-11 18:24:04 -0300

    Started ex. 5

---
+++ 5.c
@@ -0,0 +1,8 @@
+// Faça um programa para ler a nota da prova de 10 alunos e armazene num vetor, calcule 
+// e imprima a média geral (considere a média aritmética)
+
+#include <stdio.h>
+
+void main() {
+    
+}
