ad9c3efe19c8465749ba92d7d161169473b2d82e
mstewart796 <m.stewart796@gmail.com>
2023-03-11 19:53:47 -0300

    Set up ex.7

---
+++ LISTA_2_ARRAY_MATRIZ/7.c
@@ -0,0 +1,17 @@
+// Escreva um programa que leia 10 números inteiros e os armazene em um vetor. Calcule o 
+// desvio padrão e imprima na tela o resultado. 
+ 
+// O desvio padrão (DP) é calculado usando-se a seguinte fórmula: 
+ 
+// Sendo, 
+// ?: símbolo de somatório. Indica que temos que somar todos os termos, desde 
+// a primeira posição (i=1) até a posição n 
+// xi: valor na posição i no conjunto de dados 
+// MA: média aritmética dos dados 
+// n: quantidade de dados 
+
+#include <stdio.h>
+
+void main() {
+    
+}
