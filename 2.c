8618cc03275ac64a56aece1bd9b0fb371e730091
mstewart796 <m.stewart796@gmail.com>
2023-03-11 18:02:57 -0300

    Saved 2.c

---
+++ 2.c
@@ -0,0 +1,16 @@
+// Crie um programa que lê 5 valores inteiros e, em seguida, mostre na tela os valores lidos. 
+
+#include <stdio.h>
+
+void main() {
+
+    int array[5];
+    for (int i = 0; i < 5; i++) {
+        printf("Informe o %dº numero: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+    printf("Here is your array: \n");
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", array[i]);
+    }
+}
