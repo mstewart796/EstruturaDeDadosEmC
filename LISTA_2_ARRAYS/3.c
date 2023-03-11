a15d2a165df7a54cb2fd7f2b1ab2e5e140097c57
mstewart796 <m.stewart796@gmail.com>
2023-03-11 18:11:47 -0300

    Completed ex.3

--- LISTA_2_ARRAYS/3.c
+++ LISTA_2_ARRAYS/3.c
@@ -4,4 +4,13 @@
 
  void main() {
     
+    int vetor[10], counter = 0;
+    for (int i = 0; i < 10; i++) {
+        printf("Informe o %dº numero: ", i);
+        scanf("%d", &vetor[i]);
+        if (i % 2 == 0) {
+            counter++;
+        }
+    }
+    printf("O seu vetor tem %d valores pares", counter);
  }
