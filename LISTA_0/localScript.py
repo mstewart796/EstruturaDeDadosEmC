96d46aa5bba545dcd20e2c295b8331f279d19339
mstewart796 <m.stewart796@gmail.com>
2023-03-18 14:02:26 -0300

    Ran script to add portuguese language

--- LISTA_0/localScript.py
+++ LISTA_0/localScript.py
@@ -11,24 +11,24 @@ for filename in os.listdir(directory):
     if filename.endswith('.c'):
         with open(os.path.join(directory, filename), 'r') as f:
             lines = f.readlines()
-        
+
         # Check if the file already has the include line
         has_include = False
         for line in lines:
             if line.strip() == '#include <locale.h>':
                 has_include = True
                 break
-        
+
         if not has_include:
             # If the file doesn't have the include line, insert it after the stdio include
             for i, line in enumerate(lines):
                 if line.strip() == '#include <stdio.h>':
-                    lines.insert(i+1, new_line)
-            # check for void main() { to include new_line2
+                    lines.insert(i + 1, new_line)
+                # check for void/int main() { to include new_line2
                 if line.strip() == 'void main() {' or line.strip() == 'int main() {':
-                    lines.insert(i+1, new_line2)
+                    lines.insert(i + 1, new_line2)
                     break
-        
+
         # Write the modified file back to disk
         with open(os.path.join(directory, filename), 'w') as f:
             f.write(''.join(lines))
