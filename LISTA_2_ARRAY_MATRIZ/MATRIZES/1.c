4ac96a9393e837a707bf75803307e62f85479696
mstewart796 <m.stewart796@gmail.com>
2023-03-20 18:19:33 -0300

    Working on ex.1

--- LISTA_2_ARRAY_MATRIZ/MATRIZES/1.c
+++ LISTA_2_ARRAY_MATRIZ/MATRIZES/1.c
@@ -6,4 +6,20 @@
 
 void main() {
     
+    int matriz1[2][2], matriz2[2][2];
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Informe o valor de matriz1[%d][%d]: ", i, j);
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("\nmatriz1[%d][%d]: ", matriz1[i][j]);
+        }
+    }
+
+
 }
