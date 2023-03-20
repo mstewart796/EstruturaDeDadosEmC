762c22b02cfb72ae1bf3b9df4066a1d6b8ec2162
mstewart796 <m.stewart796@gmail.com>
2023-03-19 21:34:13 -0300

    Completed ex. 10

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,16 +1,20 @@
-// Faça um programa que preencha um vetor de tamanho 100 com os 100 primeiros números 
-// naturais.
-
+//  Faça um programa que preencha um vetor de tamanho 100 com os 100 primeiros números 
+// naturais que não são múltiplos de 5. 
+ 
 #include <stdio.h>
-#include <locale.h>
 
 void main() {
-    setlocale(LC_ALL, "Portuguese");
-    
-    int vetor[100];
-    for (int i = 1; i <= 100; i++) {
-        vetor[i] = i;
+
+    int vetor[100], n = 0, counter = 1;
+    while (n < 100) {
+        if (counter % 5 != 0) {
+            vetor[n] = counter;
+            n++;
+        }
+        counter++;        
+    }
+
+    for (int i = 0; i < 100; i++) {
         printf("%d\n", vetor[i]);
     }
-    
-    }
+}
