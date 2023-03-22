6355946615b6c2adbc24d56f96d626ddd568e38d
mstewart796 <m.stewart796@gmail.com>
2023-03-22 15:36:38 -0300

    Completed ex.5

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,18 +1,29 @@
-// Crie um programa que leia uma matriz 2x2 e calcule a sua determinante
-
+// Gerar e imprimir uma matriz de tamanho 10 x 10, onde seus elementos são da forma: 
+ 
+// A[i][j] = 2i + 7j - 2 se i < j; 
+// A[i][j] = 3i² + 1 se i = j; 
+// A[i][j] = 4i³ + 5j² + 1 se i > j: 
+ 
 #include <stdio.h>
 #include <locale.h>
 
 void main () {
     setlocale(LC_ALL, "Portuguese");
-    int matriz[2][2], determinante;
+    int A[10][10];
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("Informe o valor de matriz[%d][%d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (i < j) A[i][j] = 2 * i + 7 * j;
+            if (i == j) A[i][j] = 3 * (i * i) + 1;
+            if (i > j) A[i][j] = 4 * (i * i * i) + 5 * (j * j) + 1;
         }
     }
-    determinante = (matriz[0][0] * matriz[1][1]) - (matriz[0][1] * matriz[1][0]);
-    printf("A determinante é: %d", determinante);
-}
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            printf("%d\t", A[i][j]);
+        }
+        printf("\n");
+    }
+
+}    
