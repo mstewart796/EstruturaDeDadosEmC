7e553fd9c330429c446d18abc302c732a0fefb71
mstewart796 <m.stewart796@gmail.com>
2023-03-22 21:01:25 -0300

    completed ex.6

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,29 +1,19 @@
-// Gerar e imprimir uma matriz de tamanho 10 x 10, onde seus elementos são da forma: 
- 
-// A[i][j] = 2i + 7j - 2 se i < j; 
-// A[i][j] = 3i² + 1 se i = j; 
-// A[i][j] = 4i³ + 5j² + 1 se i > j: 
- 
+// Leia uma matriz de 3 x 3 elementos. Calcule a soma dos elementos que estão acima da 
+// diagonal principal. 
+
 #include <stdio.h>
 #include <locale.h>
 
-void main () {
+void main() {
     setlocale(LC_ALL, "Portuguese");
-    int A[10][10];
+    int matriz[3][3], soma;
 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (i < j) A[i][j] = 2 * i + 7 * j;
-            if (i == j) A[i][j] = 3 * (i * i) + 1;
-            if (i > j) A[i][j] = 4 * (i * i * i) + 5 * (j * j) + 1;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Informe o valor de matriz[%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
         }
     }
-
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("%d\t", A[i][j]);
-        }
-        printf("\n");
-    }
-
-}    
+    soma = matriz[0][1] + matriz[0][2] + matriz[1][2];
+    printf("A soma é: %d", soma);
+}
