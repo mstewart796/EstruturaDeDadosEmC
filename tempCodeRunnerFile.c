1b1396a24434c2099709229901d95329c5c06a79
mstewart796 <m.stewart796@gmail.com>
2023-04-14 14:39:56 -0300

    Put *A and *B into parentheses

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,19 +1,23 @@
-// Crie um programa que contenha uma matriz de float contendo 3 linhas e 3 colunas.
-// Imprima o endereço de cada posição dessa matriz.
+// Crie um programa que contenha um array de inteiros contendo 5 elementos. Utilizando
+// apenas aritmética de ponteiros, leia esse array do teclado e imprima o dobro de cada valor
+// lido.
 
 #include <stdio.h>
 #include <locale.h>
 
 void main() {
-    
+
     setlocale(LC_ALL, "Portuguese");
 
-    float myMatriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int myArray[5];
+    int *ponteiro = &myArray[0];
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            printf("O endereço de myMatriz[%d][%d] é %p\n", i, j, &myMatriz[i][j]);
-        }
+    for (int i = 0; i < 5; i++) {
+        printf("Informe o valor de myArray[%d]", i);
+        scanf("%d", myArray[(*ponteiro) + (i * 4)]);
     }
 
+    for (int i = 0; i < 5; i++) {
+        printf("O dobro da valor dentro de myArray[%d] é: \n", i, myArray[i] * 2);
+    }
 }
