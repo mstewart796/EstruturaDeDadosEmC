98376305e7dc5f6d2ca9a19a0dad9ebc7dbc3cb2
mstewart796 <m.stewart796@gmail.com>
2023-04-14 15:22:11 -0300

    Completed ex. 7

--- LISTA_4_PONTEIROS/7.c
+++ LISTA_4_PONTEIROS/7.c
@@ -6,3 +6,32 @@
 // A=4∗p∗R2
 // V =4/3∗p∗R3
 // Use p = 3.14
+
+#include <stdio.h>
+#include <locale.h>
+#define PI 3.14
+
+void calc_esfera(float R, float *area, float *volume);
+
+void main() {
+    
+    setlocale(LC_ALL, "Portuguese");
+    float R, area, volume;
+
+    printf("Informe o valor de R: ");
+    scanf("%f", &R);
+
+    calc_esfera(R, &area, &volume);
+
+    printf("A area é : %.2f\n", area);
+    printf("O volume é : %.2f\n", volume);
+
+
+}
+
+void calc_esfera(float R, float *area, float *volume) {
+
+    *area = 4 * PI * (R * R);
+    *volume = (4 / 3) * PI * (R * R * R);
+
+}
