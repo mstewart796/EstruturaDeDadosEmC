5fa04088f3ca3b3680f7faab7bd6724bfdaccd4f
mstewart796 <m.stewart796@gmail.com>
2023-04-14 14:22:02 -0300

    Completed ex. 4.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,5 @@
-// Crie um programa que contenha um array de float contendo 10 elementos. Imprima o
-// endereço de cada posição desse array.
+// Crie um programa que contenha uma matriz de float contendo 3 linhas e 3 colunas.
+// Imprima o endereço de cada posição dessa matriz.
 
 #include <stdio.h>
 #include <locale.h>
@@ -8,9 +8,12 @@ void main() {
     
     setlocale(LC_ALL, "Portuguese");
 
-    float myArray[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    float myMatriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-    for (int i = 0; i < 10; i++){
-        printf("O endereço de myArray[%d] é %p\n", i, &myArray[i]);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("O endereço de myMatriz[%d][%d] é %p\n", i, j, &myMatriz[i][j]);
+        }
     }
+
 }
