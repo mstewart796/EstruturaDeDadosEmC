1d5378606048cc31db168a5574a688f4e3a6ccda
mstewart796 <m.stewart796@gmail.com>
2023-04-16 19:16:50 -0300

    Created lista 5 and started the first exercise

---
+++ LISTA_5_STRUCTS/1.c
@@ -0,0 +1,17 @@
+//  Implemente um programa que registra o nome, matrícula, endereço e as 5 disciplinas de um 
+// aluno. Armazene estes dados em uma estrutura. 
+ 
+// Nome: Carlos Sara 
+// Matrícula: 123456 
+// Endereço: Rua do F, 11 – Recife. 
+// Disciplinas: Matemática, Português, Geografia, Física e Biologia. 
+
+#include <stdio.h>
+#include <locale.h>
+
+void main() {
+    
+    setlocale(LC_ALL, "Portuguese");
+
+
+}
