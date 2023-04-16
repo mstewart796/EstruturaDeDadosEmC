5025d1f51c56a0a2d58929d32fb50562d398bfac
mstewart796 <m.stewart796@gmail.com>
2023-04-16 20:44:55 -0300

    Changed typedef to Struct Vetor

--- LISTA_5_STRUCTS/4.c
+++ LISTA_5_STRUCTS/4.c
@@ -12,20 +12,20 @@
 #include <locale.h>
 #include <string.h>
 
-typedef struct {
+struct Vetor {
 
     float x;
     float y;
     float z;
-} Vetor;
+};
 
 void main() {
     
     setlocale(LC_ALL, "Portuguese");
 
-    Vetor vetor1 = {1.0, 2.0, 3.0}; 
-    Vetor vetor2 = {0.0, 5.0, 10.0};
-    Vetor result;
+    struct Vetor vetor1 = {1.0, 2.0, 3.0}; 
+    struct Vetor vetor2 = {0.0, 5.0, 10.0};
+    struct Vetor result;
 
     result.x = vetor1.x + vetor2.x;
     result.y = vetor1.y + vetor2.y;
