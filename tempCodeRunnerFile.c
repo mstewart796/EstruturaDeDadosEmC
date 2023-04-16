a3c18bb256e7091481669f45d1e0f1d965d061f0
mstewart796 <m.stewart796@gmail.com>
2023-04-16 20:04:41 -0300

    Completed ex. 2.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,26 +1,41 @@
-// Considere a seguinte declaração: int A, *B, **C, ***D; Escreva um programa que leia a
-// variável a e calcule e exiba o dobro, o triplo e o quádruplo desse valor utilizando apenas os
-// ponteiros B, C e D. O ponteiro B deve ser usada para calcular o dobro, C o triplo e D o
-// quádruplo.
+// Implemente um programa que leia o nome, a idade e o endereço de uma pessoa e 
+// armazene os dados em uma estrutura. Estas informações devem ser lidas do teclado.
 
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 void main() {
-
+    
     setlocale(LC_ALL, "Portuguese");
-    int A, *B, **C, ***D;
 
-    printf("Informe o valor de A: ");
-    scanf("%d", &A);
+    struct {
 
-    B = &A;
-    C = &B;
-    D = &C;
+        char nome[50];
+        int idade;
+        char endereco[50];
+    } pessoa;
 
-    printf("O valor de A é: %d\n", A);
-    printf("O valor de B é: %d\n", (*B) * 2);
-    printf("O valor de C é: %d\n", (**C) * 3);
-    printf("O valor de D é: %d\n", (***D) *4);
+    char inputNome[50];
+    int inputIdade;
+    char inputEndereco[50];
+
+    printf("Informe o seu nome: ");
+    scanf("%s", inputNome);
+
+    printf("Informe a sua idade: ");
+    scanf("%d", &inputIdade);
+
+    printf("Informe o seu endereco: ");
+    scanf("%s", inputEndereco);
+
+    strcpy(pessoa.nome, inputNome);
+    pessoa.idade = inputIdade;
+    strcpy(pessoa.endereco, inputEndereco);
+
+    printf("Nome: %s", pessoa.nome);
+    printf("Idade: %d", pessoa.idade);
+    printf("Endereco: %s", pessoa.endereco);
+
 
 }
