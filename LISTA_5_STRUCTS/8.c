02469b8e83695ae51b80cbceb4edfb047f3bd2ea
Martin Stewart <m.stewart796@gmail.com>
2023-04-20 15:35:09 -0300

    completed ex.8

--- LISTA_5_STRUCTS/8.c
+++ LISTA_5_STRUCTS/8.c
@@ -5,6 +5,7 @@
 // na estrutura e exibidos na tela. 
 
 #include <stdio.h>
+#include <string.h>
 
 typedef struct {
 
@@ -27,4 +28,25 @@ typedef struct {
 
 void main() {
 
+   Funcionario testWorker;
+
+   strcpy(testWorker.nome, "Martin");
+   testWorker.idade = 32;
+   testWorker.sexo = 'M';
+   testWorker.cpf = 123123;
+   testWorker.ddn.dia = 5;
+   testWorker.ddn.mes = 11;
+   testWorker.ddn.ano = 1990;
+   testWorker.codigo = 123;
+   strcpy(testWorker.cargo, "Programador");
+   testWorker.salario = 1234.12;
+
+   printf("Nome = %s\n", testWorker.nome);
+   printf("Idade = %d\n", testWorker.idade);
+   printf("Sexo = %c\n", testWorker.sexo);
+   printf("CPF = %d\n", testWorker.cpf);
+   printf("Data de nascimento = %d/%d/%d\n", testWorker.ddn.dia, testWorker.ddn.mes, testWorker.ddn.ano);
+    printf("Codigo = %d\n", testWorker.codigo);
+    printf("Cargo = %s\n", testWorker.cargo);
+    printf("Salario = %f\n", testWorker.salario);
 }
